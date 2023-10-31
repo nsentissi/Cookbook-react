b26118d3662136a02a6558786a93cea4351d561d
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-10-31 23:04:53 +0100

    added buttons interactivity and loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css'
 import Maindishes from './components/main-dishes'
 import Cocktails from './components/Cocktails'
-import Desserts from './components/Desserts'
+import Desserts from './components/desserts'
 
 function App() {
 
 
 
   return (
-    <>
+    <div className='main'>
      <Maindishes/>
      <Cocktails/>
      <Desserts/>
         
-    </>
+     </div>
   )
 }
 
