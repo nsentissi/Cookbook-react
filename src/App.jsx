d1a38ae0140964f36aab0f4700ae8eca9010722a
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-10-31 08:55:10 +0100

    added usecontentful and retrieved data from it

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+
+import { useState, useEffect } from 'react'
+import useContentful from './hooks/useContentful'
 
 import './App.css'
 
 function App() {
+
+  const {getRecipes} = useContentful()
+  const [recipes, setRecipes] = useState(null)
  
+  useEffect(() => {
+    getRecipes().then((data) => {
+      setRecipes(data)
+      console.log(data);
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, [])
 
   return (
     <>
-      
+      {!recipes ? <p>Loading...</p> : recipes.map((recipe) => {
+        return (
+          <div className="card">
+            <img src={recipe.img} alt={recipe.title} />
+            <div className="card-body">
+              <h5 className="card-title">{recipe.title}</h5>
+              <p className="card-text">{recipe.ingredients}</p>
+              <p className="card-text">{recipe.recipe}</p>
+              
+            </div>
+          </div>
+        )
+      })}
         
     </>
   )
