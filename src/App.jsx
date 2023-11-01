434a3b2e4ecc1f29b0ae0eec6fd13b5c6676bbe2
unknown <denis.hadzipasic@partyrent.com>
2023-11-01 12:54:18 +0100

    modified courasel

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,19 @@ import './css/sandbox.css'
 import './css/embla.css'
 import './App.css'
 
-const OPTIONS = { dragFree: true }
+const OPTIONS = {}
 const SLIDE_COUNT = 5
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 function App() {
 
-  const {getRecipes} = useContentful()
-  const [recipes, setRecipes] = useState(null)
- 
-  useEffect(() => {
-    getRecipes().then((data) => {
-      setRecipes(data)
-      console.log(data);
-    }).catch((err) => {
-      console.log(err)
-    })
-  }, [])
-
   return (
     <>
+  <main className="sandbox">
     <section className="sandbox__carousel">
       <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </section>
-        
+  </main>
     </>
   )
 }
--- src/components/EmblaCarousel.jsx
+++ src/components/EmblaCarousel.jsx
@@ -1,78 +1,91 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
-import { flushSync } from 'react-dom'
+import {
+  DotButton,
+  PrevButton,
+  NextButton
+} from './EmblaCarouselArrowsDotsButtons'
 import imageByIndex from './imageByIndex'
-
-const TWEEN_FACTOR = 1.2
 
 const EmblaCarousel = (props) => {
   const { slides, options } = props
   const [emblaRef, emblaApi] = useEmblaCarousel(options)
-  const [tweenValues, setTweenValues] = useState([])
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [scrollSnaps, setScrollSnaps] = useState([])
 
-  const onScroll = useCallback(() => {
-    if (!emblaApi) return
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi]
+  )
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi]
+  )
+  const scrollTo = useCallback(
+    (index) => emblaApi && emblaApi.scrollTo(index),
+    [emblaApi]
+  )
 
-    const engine = emblaApi.internalEngine()
-    const scrollProgress = emblaApi.scrollProgress()
+  const onInit = useCallback((emblaApi) => {
+    setScrollSnaps(emblaApi.scrollSnapList())
+  }, [])
 
-    const styles = emblaApi.scrollSnapList().map((scrollSnap, index) => {
-      let diffToTarget = scrollSnap - scrollProgress
-
-      if (engine.options.loop) {
-        engine.slideLooper.loopPoints.forEach((loopItem) => {
-          const target = loopItem.target()
-          if (index === loopItem.index && target !== 0) {
-            const sign = Math.sign(target)
-            if (sign === -1) diffToTarget = scrollSnap - (1 + scrollProgress)
-            if (sign === 1) diffToTarget = scrollSnap + (1 - scrollProgress)
-          }
-        })
-      }
-      return diffToTarget * (-1 / TWEEN_FACTOR) * 100
-    })
-    setTweenValues(styles)
-  }, [emblaApi, setTweenValues])
+  const onSelect = useCallback((emblaApi) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap())
+    setPrevBtnDisabled(!emblaApi.canScrollPrev())
+    setNextBtnDisabled(!emblaApi.canScrollNext())
+  }, [])
 
   useEffect(() => {
     if (!emblaApi) return
-    onScroll()
-    emblaApi.on('scroll', () => {
-      flushSync(() => onScroll())
-    })
-    emblaApi.on('reInit', onScroll)
-  }, [emblaApi, onScroll])
+
+    onInit(emblaApi)
+    onSelect(emblaApi)
+    emblaApi.on('reInit', onInit)
+    emblaApi.on('reInit', onSelect)
+    emblaApi.on('select', onSelect)
+  }, [emblaApi, onInit, onSelect])
 
   return (
-    <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {slides.map((index) => (
-            <div className="embla__slide" key={index}>
-              <div className="embla__slide__number">
-                <span>{index + 1}</span>
-              </div>
-              <div className="embla__parallax">
-                <div
-                  className="embla__parallax__layer"
-                  style={{
-                    ...(tweenValues.length && {
-                      transform: `translateX(${tweenValues[index]}%)`
-                    })
-                  }}
-                >
-                  <img
-                    className="embla__slide__img embla__parallax__img"
-                    src={imageByIndex(index)}
-                    alt="Your alt text"
-                  />
+    <>
+      <div className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {slides.map((index) => (
+              <div className="embla__slide" key={index}>
+                <div className="embla__slide__number">
+                  <span>{index + 1}</span>
                 </div>
+                <img
+                  className="embla__slide__img"
+                  src={imageByIndex(index)}
+                  alt="Your alt text"
+                />
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
+
+        {/* <div className="embla__buttons">
+          <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
+          <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
+        </div> */}
       </div>
-    </div>
+
+      <div className="embla__dots">
+        {scrollSnaps.map((_, index) => (
+          <DotButton
+            key={index}
+            onClick={() => scrollTo(index)}
+            className={'embla__dot'.concat(
+              index === selectedIndex ? ' embla__dot--selected' : ''
+            )}
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
