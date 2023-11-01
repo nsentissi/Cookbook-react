e6b0c9a48b500a580a95b6114eee98746e8e483d
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-11-01 11:05:50 +0100

    fixed issue with state, create a card component with its own state

---
+++ src/components/Recipe-card.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const RecipeCard = ({ recipe }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [liked, setLike] = useState(false);
+
+  const handleLike = () => {
+    setLike(!liked);
+  };
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ width: 600, marginBottom: 3 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <ExpandMoreIcon />
+          </IconButton>
+        }
+        title={recipe.title}
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        component="img"
+        height="350"
+        image={recipe.img}
+        alt={recipe.title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Ingredients : {recipe.ingredients}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon onClick={handleLike} style={liked ? { color: 'red' } : {}} />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Recipe:</Typography>
+          <Typography paragraph>{recipe.recipe}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
+
+export default RecipeCard;
